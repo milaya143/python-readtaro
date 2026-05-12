ae3df76934317103e7e552d5d7c874a3ddddba95
milaya143 <cloudsia8@gmail.com>
2026-05-12 21:22:02 +0700

    Update tarot_bot.py

--- tarot_bot.py
+++ tarot_bot.py
@@ -7,6 +7,21 @@ Admin notifications → @ontobe
 import os
 import logging
 import asyncio
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=10000) # Render обычно ищет порт 10000
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 from telegram import (
     Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 )
